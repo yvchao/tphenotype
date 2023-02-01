11b46d54e4c0161d037b0fffc74a6d82d1ae9b15
Yuchao Qin <yvchao@outlook.com>
2023-02-01 23:34:15 +0000

    experiment: add encoder hparams selection

---
+++ notebooks/benchmark/benchmark.py
@@ -0,0 +1,170 @@
+import torch
+import pandas as pd
+import numpy as np
+from numpy.core.multiarray import interp
+from tqdm import auto
+
+from tphenotype.utils import get_auc_scores, get_cls_scores, select_by_steps, data_split
+
+
+def batch_interp(t, tp, fp):
+    return np.stack([interp(t, tp[i], fp[i]) for i in range(fp.shape[0])], axis=0)
+
+
+def data_interpolation(t, x, samples=20):
+    times = np.linspace(0, 1, num=samples)
+    _, _, x_dim = x.shape
+    xs = []
+    for i in range(x_dim):
+        x_interp = batch_interp(times, t, x[:, :, i])
+        xs.append(x_interp)
+    xs = np.stack(xs, axis=-1)
+    return xs
+
+
+def evaluate(model, dataset, steps=[-1]):
+    x = dataset['x']
+    t = dataset['t']
+    c = dataset.get('c', None)
+    y = dataset['y']
+    mask = dataset['mask']
+
+    try:
+        c_pred = model.predict_cluster(x, t, mask, steps)
+        y_pred = model.predict_proba(x, t, mask, steps)
+    except:
+        try:
+            c_pred = model.predict_cluster(x, t)
+            c_pred = select_by_steps(c_pred, mask, steps)
+        except:
+            c_pred = None
+
+        try:
+            y_pred = model.predict_proba(x, t)
+            y_pred = select_by_steps(y_pred, mask, steps)
+
+        except:
+            y_pred = None
+
+    if c is not None:
+        c_true = select_by_steps(c, mask, steps)
+    else:
+        c_true = None
+
+    y_true = select_by_steps(y, mask, steps)
+
+    if y_pred is not None:
+        AUROC, AUPRC = get_auc_scores(y_true, y_pred)
+        auc_scores = {'ROC': np.mean(AUROC), 'PRC': np.mean(AUPRC)}
+    else:
+        auc_scores = {}
+
+    if c_pred is not None:
+        x_sel = select_by_steps(x, mask, steps, sub_sequence=True, keepdims=True)
+        t_sel = select_by_steps(t, mask, steps, sub_sequence=True, keepdims=False)
+        x_interp = data_interpolation(t_sel, x_sel)
+        cls_scores = get_cls_scores(c_true=c_true, c_pred=c_pred, x=x_interp, y_true=y_true)
+    else:
+        cls_scores = {}
+
+    mixed1 = 2/ (1/auc_scores.get('ROC',1e-10) + 1/cls_scores.get('Silhouette_auc',1e-10))
+    mixed2 = 2/ (1/auc_scores.get('PRC',1e-10) + 1/cls_scores.get('Silhouette_auc',1e-10))
+    scores = {'method': model.name, **auc_scores, **cls_scores, 'Hroc':mixed1,'Hprc':mixed2}
+    return scores
+
+
+def get_ci(series, decimals=3):
+    if series.dtype == 'object':
+        return series.iloc[0]
+
+    stats = series.agg(['mean', 'sem'])
+    mean = np.format_float_positional(stats['mean'], decimals)
+    ci = np.format_float_positional(1.96 * stats['sem'], decimals)
+    out = f'{mean}+-{ci}'
+    return out
+
+
+def benchmark_old(method, config, dataset, loss_weights, steps=[-1], epochs=50, n=3, seed=0):
+    dtype='float32'
+    
+    #train_set, valid_set, test_set = dataset
+    #N, T, _ = dataset['x'].shape
+
+    results = []
+    for i in auto.tqdm(range(n), desc=f'{method.__name__}'):
+        train_set, test_set = data_split(dataset, test_size=0.2, random_state=seed+i, dtype=dtype)
+        train_set, valid_set = data_split(train_set, test_size=0.2, random_state=seed+i, dtype=dtype)
+    
+        torch.random.manual_seed(seed+i)
+        model = method(**config)
+        model = model.fit(train_set, loss_weights, valid_set=valid_set, epochs=epochs, verbose=False)
+        scores = evaluate(model, test_set, steps)
+        results.append(scores)
+    results = pd.DataFrame(results)
+    summary = results.apply(get_ci)
+    return summary
+
+def benchmark(method, config, splits, loss_weights, steps=[-1], epochs=50, seed=0):
+    dtype='float32'
+    
+
+    results = []
+    for i,dataset in auto.tqdm(enumerate(splits), total=len(splits), desc=f'{method.__name__}'):
+        train_set, valid_set, test_set = dataset
+    
+        torch.random.manual_seed(seed+i)
+        model = method(**config)
+        model = model.fit(train_set, loss_weights, valid_set=valid_set, epochs=epochs, verbose=False)
+        scores = evaluate(model, test_set, steps)
+        results.append(scores)
+    results = pd.DataFrame(results)
+    summary = results.apply(get_ci)
+    return summary
+
+
+KME2P_config = {
+    'K': None,
+    'x_dim': None,
+    'y_dim': None,
+    'latent_size': 10,
+    'hidden_size': 10,
+    'num_layers': 2,
+}
+
+Encoder_config = {
+    'num_poles': 4,    # number of poles
+    'max_degree': 1,    # maximum degree of poles
+    'hidden_size': 10,    # number of hidden units in neural networks
+    'num_layers': 1,    # number of layers in MLP of the encoder (1 layer RNN + n layer MLP)
+    'pole_separation': 1.0,    # minimum distance between distinct poles 
+    'freq_scaler': 30,    # scale up the imaginary part to help learning
+    'window_size': None,    # whether or not to include time delay terms
+    'equivariant_embed': True,    # whether or not to sort the poles (useless during training)
+}
+
+Cls_config = {
+    'K': None,
+    'steps': [-1],
+    'tol': 1e-6,
+    'test_num': 50,
+}
+
+Predictor_config = {
+    'x_dim': None,
+    'y_dim': None,
+    'time_series_dims': None,
+    'hidden_size': 10,
+    'num_layer': 3,
+    'global_bias': False,
+    'encoder_config': None,
+    'cls_config': None,
+    'categorical': True,
+}
+
+loss_weights = {
+    'ce': 1.0,
+    'rmse': 1.0,
+    'cont':0.01,
+    'pole': 1.0,
+    'real': 0.1
+}
---
+++ notebooks/benchmark/hs_encoder.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pickle
+from tqdm import auto
+import numpy as np
+import pandas as pd
+import itertools
+import torch
+import os
+import json
+from tphenotype import LaplaceEncoder, Predictor
+from tphenotype.baselines import E2P, KME2P, KMDTW
+from tphenotype.utils import get_auc_scores, get_cls_scores, select_by_steps
+from benchmark import benchmark, KME2P_config, Predictor_config, Encoder_config, Cls_config, loss_weights
+
+
+
+def evaluate_encoder(method, config, loss_weights, splits,seed=0, epochs=50):
+    results = []
+    for i,dataset in auto.tqdm(enumerate(splits), total=len(splits), desc=f'{method.__name__}'):
+        train_set, valid_set, test_set = dataset
+
+        torch.random.manual_seed(seed+i)
+        model = method(**config)
+        mse = model.evaluate_encoder_params(train_set, test_set, loss_weights, valid_set=valid_set, epochs=epochs, verbose=False)
+        results.append(mse)
+    results = np.array(results)
+    return results, model
+
+os.makedirs('hyperparam_selection', exist_ok=True)
+
+
+
+# In[5]:
+
+
+def load_data(dataname, verbose=False):
+    with open(f'data/{dataname}_data_hs.pkl', 'rb') as file:
+        splits = pickle.load(file)
+        
+    if dataname == 'Synth':
+        feat_list=['x1','x2']
+        temporal_dims = [0,1]
+    elif dataname == 'ADNI':
+        feat_list = ['APOE4','CDRSB','Hippocampus']
+        temporal_dims = [1,2]
+    elif dataname == 'ICU':
+        feat_list = ['Age', 'Gender', 'GCS', 'PaCO2']
+        temporal_dims = [2, 3]
+    else:
+        raise ValueError(f'unknown dataset {dataname}')
+    
+    if verbose:
+        tr_set,va_set,te_set = splits[0]
+        _, T, x_dim = tr_set['x'].shape
+        _, _, y_dim = tr_set['y'].shape
+        print(dataname)
+        print(f"total samples: {len(tr_set['x'])+len(va_set['x'])+len(te_set['x'])}")
+        
+        print(f'max length: {T}')
+        print(f'x_dim: {x_dim}')
+        print(f'y_dim: {y_dim}')
+
+        print(f'features: {feat_list}')
+        print(f'temporal dims: {temporal_dims}')
+    return splits, feat_list, temporal_dims
+
+
+# In[7]:
+
+
+def hyperparam_selection_encoder(dataname,seed=0, epochs = 50):
+    splits, feat_list, temporal_dims = load_data(dataname, verbose=True)
+    tr_set,va_set,te_set = splits[0]
+    _, T, x_dim = tr_set['x'].shape
+    _, _, y_dim = tr_set['y'].shape
+
+    # Configuration
+    K=1
+
+    encoder_config = Encoder_config.copy()
+    encoder_config['window_size']=None
+    encoder_config['pole_separation']=2.0
+    encoder_config['freq_scaler']=20
+
+    cls_config = Cls_config.copy()
+    cls_config['K'] = K
+    cls_config['steps'] = [-1]
+    predictor_config = Predictor_config.copy()
+    predictor_config['x_dim'] = x_dim
+    predictor_config['y_dim'] = y_dim
+    predictor_config['time_series_dims'] = temporal_dims
+    predictor_config['cls_config'] = cls_config
+    predictor_config['encoder_config'] = encoder_config
+
+    loss_weights['cont']=0.01
+
+    print('T_phenotype config:')
+    print(predictor_config)
+
+    scores = pd.DataFrame(columns=['mse_mean','mse_std', 'config'])
+    for i,comb in enumerate(itertools.product(*search_space.values())):
+        test_config = encoder_config.copy()
+        test_loss_weights = loss_weights.copy()
+        msg = []
+        for j,k in enumerate(search_space.keys()):
+            if k in encoder_config:
+                test_config[k] = comb[j]
+            elif k in loss_weights:
+                test_loss_weights[k]=comb[j]
+            msg.append(f'{k}:{comb[j]}')
+        predictor_config['encoder_config'] = test_config
+        msg = ','.join(msg)
+        print(f'test config {msg} ...')
+        results, model = evaluate_encoder(Predictor,predictor_config,test_loss_weights,splits,seed=seed, epochs=epochs)
+        scores.loc[i,'mse_mean'] = np.mean(results)
+        scores.loc[i,'mse_std'] = np.std(results)
+        scores.loc[i,'config'] = json.dumps(loss_weights)
+        scores.to_csv(f'hyperparam_selection/{dataname}_encoder.csv')
+
+    scores = pd
+    scores = scores.astype({'mse_mean':'float'})
+    best = scores['mse_mean'].idxmin()
+    print('Optimal hyperparameters:')
+    print(scores.loc[best,'config'])
+
+
+# In[8]:
+# In[4]:
+
+
+loss_weights = {
+    'rmse': 1.0,
+    'cont':0.01,
+    'pole': 1.0,
+    'real': 0.1
+}
+
+search_space = {
+    'pole':[1.0,10.0],
+    'real':[0.1,1.0],
+    'pole_separation':[1.0, 2.0],
+    'max_degree': [1,2],
+}
+
+
+
+for dataname in ['Synth', 'ICU', 'ADNI']:
+    hyperparam_selection_encoder(dataname)
+
+
+# In[ ]:
+
+
+
+
