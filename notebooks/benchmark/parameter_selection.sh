633d5782add948feacd943785ddc38c7abd9b959
Yuchao Qin <yvchao@outlook.com>
2023-02-16 21:50:15 +0000

    experiment: add script for hparams selection

---
+++ notebooks/benchmark/parameter_selection.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+
+echo "hparams selection for TPhenotype"
+echo "encoders"
+python hs_encoder.py
+echo "predictor"
+python hs_predictor.py
+echo "K"
+python hs_K.py
+
+echo "hparams selection for baselines"
+echo "E2Py"
+python hs_e2py.py 
+echo "E2Pz"
+python hs_e2pz.py 
+echo "Spectral"
+python hs_spectral.py 
