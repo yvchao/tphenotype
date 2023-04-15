3fec937094019a0b1c725d60cfe471f2e86f9fa3
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-04-15 21:04:40 +0100

    Update script

--- notebooks/benchmark/run_experiment.sh
+++ notebooks/benchmark/run_experiment.sh
@@ -3,9 +3,25 @@
 # use deterministic algorithms
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-# Uncomment the below two lines to re-run data splitting and hyperparameter selection:
+# Uncomment the below lines if re-running data splitting and hyperparameter selection:
 # rm -rfv ./data/*
 # rm -rfv ./hyperparam_selection/*
+
+# Remove the previous benchmark results.
+# Uncomment to make sure to re-run all benchmarks from scratch.
+# rm -rfv ./benchmark_results/*
+
+# Remove model caches - model loading does not currently work correctly.
+rm -rfv ./model_cache/*
+rm -rfv ./external/ACTPC/ADNI/*
+rm -rfv ./external/ACTPC/ICU/*
+rm -rfv ./external/ACTPC/Synth/*
+rm -rfv ./external/ACTPC/output/*
+rm -rfv ./external/dcn_Seq2Seq/ADNI/*
+rm -rfv ./external/dcn_Seq2Seq/ICU/*
+rm -rfv ./external/dcn_Seq2Seq/Synth/*
+rm -rfv ./external/dcn_Seq2Seq/output/*
+# ----------------------------------------------------------------------
 
 echo  "Split datasets..."
 python -u data_split.py
